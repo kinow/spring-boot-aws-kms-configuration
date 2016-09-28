69fc966efda3131118913d452f3cecdd2e693b90
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-09-29 10:03:37 +1300

    Adding Spring Boot and KMS classes

---
+++ README.txt
@@ -0,0 +1,3 @@
+Simple project displaying how to use Spring Boot with KMS encryption.
+
+Integration provided using https://github.com/zalando/spring-cloud-config-aws-kms
---
+++ src/main/java/test/aws/kms/configuration/Application.java
@@ -0,0 +1,39 @@
+package test.aws.kms.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@RestController
+@ComponentScan(basePackages = "test.aws.kms")
+public class Application {
+
+	@SuppressWarnings("unused")
+	@Autowired
+	private TextEncryptor encryptor;
+
+	@Value("${not.secret}")
+	private String notSecret;
+
+	@Value("${yes.secret}")
+	private String yesSecret;
+
+	@RequestMapping("/")
+	public String testConfiguration() {
+		// values that start with {cipher} are automatically decrypted. Or you
+		// can use the injected encryptor to encrypt/decrypt text on the fly.
+		//String decrypted = encryptor.decrypt("Another secret");
+		return String.format("Not a secret = %s<br/>Secret = %s", notSecret, yesSecret);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
