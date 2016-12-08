2e1d46513274d1384b7f91ee4da1697cdfa9b7c3
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-12-08 15:20:15 +1300

    Add Servlet 3.0 initializer

---
+++ src/main/java/test/aws/kms/configuration/ServletInitializer.java
@@ -0,0 +1,15 @@
+package test.aws.kms.configuration;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+}
