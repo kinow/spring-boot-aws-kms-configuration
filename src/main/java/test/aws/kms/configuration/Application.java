926e9cf9de8b2ed6ced2a907df6b07c269e2f7bf
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-12-08 15:21:19 +1300

    Formatting and simplify code

--- src/main/java/test/aws/kms/configuration/Application.java
+++ src/main/java/test/aws/kms/configuration/Application.java
@@ -1,11 +1,9 @@
 package test.aws.kms.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.crypto.encrypt.TextEncryptor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @ComponentScan(basePackages = "test.aws.kms")
 public class Application {
 
-	@SuppressWarnings("unused")
-	@Autowired
-	private TextEncryptor encryptor;
+//	@SuppressWarnings("unused")
+//	@Autowired
+//	private TextEncryptor encryptor;
 
 	@Value("${not.secret}")
 	private String notSecret;
@@ -29,7 +27,7 @@ public class Application {
 		// values that start with {cipher} are automatically decrypted. Or you
 		// can use the injected encryptor to encrypt/decrypt text on the fly.
 		//String decrypted = encryptor.decrypt("Another secret");
-		return String.format("Not a secret = %s<br/>Secret = %s", notSecret, yesSecret);
+		return String.format("No Secret = %s<br/>Secret = %s", notSecret, yesSecret);
 	}
 
 	public static void main(String[] args) {
