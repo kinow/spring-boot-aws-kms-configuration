511f1f6072105e50622fd91f292a52479cb30087
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2017-02-10 11:11:14 +1300

    Update to latest Spring Boot, Spring, and use new spring-cloud-aws-kms

--- src/main/java/test/aws/kms/configuration/ServletInitializer.java
+++ src/main/java/test/aws/kms/configuration/ServletInitializer.java
@@ -1,7 +1,7 @@
 package test.aws.kms.configuration;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
